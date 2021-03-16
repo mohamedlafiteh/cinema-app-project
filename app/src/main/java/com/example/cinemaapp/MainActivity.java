e3117f062cd9c3735d0ac3f4c221ba27e23381a9
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-03-16 19:00:03 +0000

    Add design

--- app/src/main/java/com/example/cinemaapp/MainActivity.java
+++ app/src/main/java/com/example/cinemaapp/MainActivity.java
@@ -3,25 +3,47 @@ package com.example.cinemaapp;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    Spinner spinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Spinner cinemaSpinner;
+
         super.onCreate(savedInstanceState);
-
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-
         getSupportActionBar().setCustomView(R.layout.tootbar_title);
 
         setContentView(R.layout.activity_main);
 
+    }
+
+    /**
+     * Lunch the login page activity .
+     */
+    public void lunchLoginActivity(View v){
+        // lunch the activity
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Lunch the account page activity .
+     */
+    public void lunchAccountActivity(View v){
+        // lunch the activity
+        Intent intent = new Intent(this, AccountActivity.class);
+        startActivity(intent);
     }
 
 //    private void showList(){
